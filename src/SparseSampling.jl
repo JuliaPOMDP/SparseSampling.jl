e56b55e551b9c18f696058fa2d46ec52ecdc8573
Zachary Sunberg <sunbergzach@gmail.com>
2019-07-26 17:48:56 -0700

    implemented weighted (but no tests yet)

--- src/SparseSampling.jl
+++ src/SparseSampling.jl
@@ -1,62 +1,41 @@
 module SparseSampling
 
-struct SSOptions
+using Random
+using POMDPs
+using Parameters
+
+abstract type AbstractSSOptions end
+
+# accessors
+maxdepth(opt::AbstractSSOptions) = opt.maxdepth
+width(opt::AbstractSSOptions) = opt.width
+
+@with_kw struct SSOptions <: AbstractSSOptions
     maxdepth::Int
     width::Int
 end
 
-struct SSPlanner{M<:Union{MDP,POMDP},R<:AbstractRNG}
+@with_kw struct POWSSOptions <: AbstractSSOptions
+    maxdepth::Int
+    width::Int
+end
+
+struct SparseSamplingSolver
+    opt::AbstractSSOptions
+    rng::AbstractRNG
+end
+
+SparseSamplingSolver(opt::AbstractSSOptions; rng=Random.GLOBAL_RNG) = SparseSamplingSolver(SSOptions(kwargs...), rng)
+SparseSamplingSolver(;rng=Random.GLOBAL_RNG, kwargs...) = SparseSamplingSolver(SSOptions(kwargs...), rng)
+
+POMDPs.solve(s::SparseSamplingSolver, m::Union{MDP,POMDP}) = SSPlanner(m, s.opt, s.rng)
+
+struct SSPlanner{M<:Union{MDP,POMDP},O<:AbstractSSOptions,R<:AbstractRNG}
     m::M
-    opt::SSOptions
+    opt::O
     rng::R
 end
 
-function estimate_v(m::POMDP, belief::Vector, opt::SSOptions, rng::AbstractRNG)
-    if depth(h) == p.maxdepth
-        return 0.0
-    end
-
-    vstar = -Inf
-    for a in actions(m)
-        q = estimate_q(m, belief, a, opt, rng)
-        vstar = max(q, vstar)
-    end
-
-    return vstar
-end
-
-function estimate_q(m::POMDP, belief::Vector, a, opt::SSOptions, rng::AbstractRNG)
-    q = 0.0
-    children = Dict{obstype(m), Vector{statetype(m)}}()
-
-    for i in 1:p.width
-        s = belief[mod1(i, length(belief))]
-        sp, o, r = generate_sor(m, s, a, p.rng)
-        q += r
-        if haskey(children, o)
-            push!(children[o], s)
-        else
-            children[o] = [s]
-        end
-    end
-
-    for c in values(children)
-        vp = estimate_v(p, m, c)
-        q += discount(m)*vp*length(c)/p.width
-    end
-    return q
-end
-
-function action(p::SSPlanner, b)
-    qstar = -Inf
-    astar::actiontype(p.m)
-    belief = collect(rand(rng, b) for i in 1:p.opt.width)
-    for a in actions(p.m)
-        if estimate_q(p.m, belief, a, p.opt, p.rng) >= qstar
-            astar = a
-        end
-    end
-    return astar
-end
+include("pomdp_sparse_sampling.jl")
 
 end # module
